2c22a74a5259cbed0f4b2915088edbaa1f1d6457
Travis Cook <travismcook93@gmail.com>
2022-01-28 20:29:18 -0700

    fixing file structure/server file and routes defined

---
+++ server/server.js
@@ -0,0 +1,35 @@
+// variable declarations
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const PORT = process.env.PORT || 3001
+const routes = require('./routes')
+
+//defining middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+// allows routing for both html and api routes
+app.use(routes)
+
+//gets the database connected and ready to listen
+const connection = process.env.MONGODB_URI || 'mongodb://localhost/ecommerce'
+mongoose.connect(connection, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log("database now running!")
+}).catch(err =>{
+  console.log(err)
+})
+
+//gets the app listening
+app.listen(PORT, () =>{
+  console.log('App now listening on port 3001')
+})
