82b1cc9b14221ebc97d6c43efd8c37d1bafe0438
Travis Cook <travismcook93@gmail.com>
2022-01-31 11:28:06 -0700

    user account deletion route created

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,5 @@
-const db = require('../models/index')
+const db = require('../models/index');
+const { findOne } = require('../models/userModel');
 
 module.exports = {
   create: (req, res) => {
@@ -6,5 +7,18 @@ module.exports = {
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
+  },
+  findOne: (req, res) => {
+    db.User
+    .findOne(req.email)
+    .then(dbModel => res.json(dbModel))
+    .catch(err => res.status(422).json(err))
+  },
+  remove: (req, res) => {
+      db.User
+        .findOne({ email: req.body.email })
+        .then(dbModel => dbModel.remove())
+        .then(dbModel => res.json(dbModel))
+        .catch(err => res.status(422).json(err));
   }
 }
--- server/routes/apiRoutes/userRoutes.js
+++ server/routes/apiRoutes/userRoutes.js
@@ -5,5 +5,6 @@ const userController = require('../../controllers/userController')
 // this is the signup route
 router.route('/signup')
   .post(userController.create)
-
+  .get(userController.findOne)
+  .delete(userController.remove)
 module.exports = router
